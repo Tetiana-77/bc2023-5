3a07e0314185e975b68d30983522335d94515282
Tetiana-77 <144829121+Tetiana-77@users.noreply.github.com>
2023-11-27 16:57:16 +0200

    index.js

--- index.js
+++ index.js
@@ -1,70 +1,77 @@
 const express = require('express');
 const fs = require('fs');
 const multer = require('multer');
-const upload = multer();
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 8000;
 
 app.use(multer().none());
 app.use(express.json());
 app.use(express.static('static'));
+app.use(bodyParser.raw({ type: 'text/plain' }));
 
-const notes = [];
+let filename = 'notes.json';
 
-app.get('/', (req, res) => {
-  res.send('Сервіс запущено. Вітаємо!');
+if (!fs.existsSync(filename)) {
+  fs.writeFileSync(filename, '[]', 'utf8');
+}
+
+const notes = JSON.parse(fs.readFileSync(filename, 'utf8'));
+
+function writefile() {
+  fs.writeFileSync(filename, JSON.stringify(notes), 'utf8');
+}
+
+app.get('/notes', (req, res) => {
+  res.json(notes);
 });
 
 app.get('/UploadForm.html', (req, res) => {
   res.sendFile(__dirname + '/static/UploadForm.html');
 });
 
-app.get('/notes', (req, res) => {
-  res.json(notes);
-});
+app.post('/upload', (req, res) => {
+  const note_name = req.body.note_name;
+  const note = req.body.note;
+  const existing = notes.find(note => note.note_name === note_name);
+
+  if (existing) {
+    res.status(400).send("This note already exist");
+  } else {
+    notes.push({ note_name: note_name, note: note });
+
+  writefile();
+
+
+    res.status(201).send("Created");
+  } 
+}); 
+
 
 app.get('/notes/:note_name', (req, res) => {
   const note_name = req.params.note_name;
   const findnote = notes.find(note => note.note_name === note_name);
 
   if (findnote) {
-    res.send({ note_name: note_name, note: findnote.note });
+    res.send(findnote.note);
   } else {
     res.status(404).send('Not Found');
   }
 });
 
-app.post('/upload', (req, res) => {
-  const note_name = req.body.note_name;
-  const note = req.body.note;
-
-  const existing = notes.find(note => note.note_name === note_name);
-
-  if (existing) {
-    res.status(400).send("Bad Request");
-  } else {
-    notes.push({ note_name: note_name, note: note });
-
-    // Зберігаємо оновлені нотатки у форматі JSON
-    fs.writeFileSync('notes.json', JSON.stringify(notes), 'utf8');
-
-    res.status(201).send("OK");
-  }
-});
-
-app.put('/notes/:note_name', (req, res) => {
-  const note_name = req.params.note_name;
-  const note = req.body.note;
-  const noteIndex = notes.findIndex(note => note.note_name === note_name);
+app.put("/notes/:noteName", (req, res) => {
+  const noteName = req.params.noteName;
+  const noteIndex = notes.findIndex(note => note.note_name === noteName);
 
   if (noteIndex !== -1) {
-    notes[noteIndex].note = note;
-    fs.writeFileSync('notes.json', JSON.stringify(notes), 'utf8');
-    res.status(200).send("OK");
+    notes[noteIndex].note = req.body;
+    res.sendStatus(200);
   } else {
-    res.status(404).json("Not Found");
+    res.sendStatus(404);
   }
 });
+
 
 app.delete('/notes/:note_name', (req, res) => {
   const note_name = req.params.note_name;
@@ -72,13 +79,13 @@ app.delete('/notes/:note_name', (req, res) => {
 
   if (noteIndex !== -1) {
     notes.splice(noteIndex, 1);
-    fs.writeFileSync('notes.json', JSON.stringify(notes), 'utf8');
-    res.status(200).send('OK');
+    writefile()
+    res.status(200).send('Note deleted');
   } else {
     res.status(404).send('Not Found');
   }
 });
 
 app.listen(port, () => {
-  console.log(`Сервер працює на порту ${port}`);
+  console.log(`The server is running on the port ${port}`);
 });
